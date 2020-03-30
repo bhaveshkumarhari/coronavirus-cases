4f480b74916797f2f1ca4630e066aaad8e28ea9d
bhaveshkumarhari <bhaveshkumar.hari@gmail.com>
2020-03-30 19:34:30 +0100

    added uk cases

--- core/views.py
+++ core/views.py
@@ -4,17 +4,18 @@ import requests
 from bs4 import BeautifulSoup
 
 import re
-
+import pandas
 import json
 
+r = requests.get("https://www.worldometers.info/coronavirus/")
+c = r.content
+
+soup = BeautifulSoup(c,"html.parser")
+
+all = soup.find_all("div",{"class":"container"})[1]
+
+
 def cases(request):
-
-    r = requests.get("https://www.worldometers.info/coronavirus/")
-    c = r.content
-
-    soup = BeautifulSoup(c,"html.parser")
-
-    all = soup.find_all("div",{"class":"container"})[1]
 
     third_div_row = all.find_all("div",{"class":"row"})[2]
     first_div_grid = third_div_row.find_all("div",{"class":"col-md-8"})[0]
@@ -148,32 +149,106 @@ def cases(request):
     return render(request, "dashboard.html", context)
 
 def united_kingdom_cases(request):
-    url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
 
-    querystring = {"country":"United Kingdom"}
+    fourth = all.find_all("div",{"class":"row"})[3]
+    first_div_col = fourth.find_all("div",{"class":"col-md-8"})[0]
+    first_innercontent = first_div_col.find_all("div",{"id":"innercontent"})[0]
+    first_div_row = first_innercontent.find_all("div",{"class":"row"})[0].text
+    # news_block = first_div_row.find_all("div",{"class":"news_post"})
 
-    headers = {
-        'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
-        'x-rapidapi-key': "b366e49ea8mshd793e280f2aa844p1f4bc1jsn63e668661ccf"
-        }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    news_list = []
+    news_list = first_div_row.splitlines()
 
-    data_json = json.loads(response.text)['data']
-    covid_stats = data_json['covid19Stats']
-    last_covid_stats = covid_stats[-1]
+    for item in news_list:
+        news_list.remove('')
+    
+    news_list.remove('Latest Updates')
+    some_news_list = news_list[:50]
 
-    for key, value in last_covid_stats.items():
-        if key == 'confirmed':
-            uk_total_cases = value
-        if key == 'deaths':
-            uk_total_deaths = value
-        if key == 'recovered':
-            uk_total_recovered = value
+    plain_news_list = []
+    for item in some_news_list:
+        removed_source = item.replace('[source]','')
+        plain_news_list.append(removed_source)
+    
+#-------------------------------------------------------------------------
 
-    context = {'uk_total_cases': uk_total_cases, 'uk_total_deaths': uk_total_deaths, 'uk_total_recovered': uk_total_recovered}
+    # url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
+
+    # querystring = {"country":"United Kingdom"}
+
+    # headers = {
+    #     'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
+    #     'x-rapidapi-key': "b366e49ea8mshd793e280f2aa844p1f4bc1jsn63e668661ccf"
+    #     }
+
+    # response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # data_json = json.loads(response.text)['data']
+    # covid_stats = data_json['covid19Stats']
+    # last_covid_stats = covid_stats[-1]
+
+    third_div_row = all.find_all("div",{"class":"row"})[2]
+    first_div_grid = third_div_row.find_all("div",{"class":"col-md-8"})[0]
+    first_div_content = first_div_grid.find_all("div",{"class":"tab-content"})[0]
+    first_div_active = first_div_content.find_all("div",{"class":"tab-pane active"})[0]
+    first_div_countries = first_div_active.find_all("div",{"class":"main_table_countries_div"})[0]
+    first_table = first_div_countries.find_all("table",{"id":"main_table_countries_today"})[0]
+    first_table_body = first_table.find_all("tbody")[0]
+    country = first_table_body.find_all("tr")
+
+
+    lst = []
+    for item in country:
+        data = {}
+        data["Country"] = item.find_all("td")[0].text
+        data["Total_Cases"] = item.find_all("td")[1].text
+        data["New_Cases"] = item.find_all("td")[2].text
+        data["Total_Deaths"] = item.find_all("td")[3].text.replace(" ","")
+        data["New_Deaths"] = item.find_all("td")[4].text
+        data["Total_Recovered"] = item.find_all("td")[5].text
+        data["Active_Cases"] = item.find_all("td")[6].text
+        data["Critical"] = item.find_all("td")[7].text
+        # data["Tot Cases/ 1M pop"] = item.find_all("td")[8].text
+        lst.append(data)
+
+    df=pandas.DataFrame(lst)
+    uk_value = df[df['Country'] == 'UK']
+    uk_total_cases = uk_value.Total_Cases.to_string(index=False)
+    uk_total_deaths = uk_value.Total_Deaths.to_string(index=False)
+    uk_total_recovered = uk_value.Total_Recovered.to_string(index=False)
+    uk_total_critical = uk_value.Critical.to_string(index=False)
+
+
+#-----------------------------------------------------------------------------------
+
+    r_uk = requests.get("https://www.portsmouth.co.uk/health/coronavirus/coronavirus-full-list-places-covid-19-cases-england-and-how-many-have-tested-positive-uk-so-far-2457616", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+    c_uk = r_uk.content
+
+    uk_soup = BeautifulSoup(c_uk,"html.parser")
+
+    uk_all = uk_soup.find_all("div",{"id":"content-wrapper"})[0]
+
+    uk_data = uk_all.find_all("div",{"class":"markup sc-bbkauy leZDRt"})
+
+
+    uk_list = []
+    for count in range(11,160):
+        dict_data = {}
+        ukdata = uk_data[count].text
+        list_data = ukdata.split(': ')
+
+        dict_data["City"] = list_data[0]
+        dict_data["Cases"] = list_data[1]
+        
+        uk_list.append(dict_data)
+
+    context = {'uk_list': uk_list, 'plain_news_list': plain_news_list, 'uk_total_cases': uk_total_cases, 'uk_total_deaths': uk_total_deaths, 'uk_total_recovered': uk_total_recovered, 'uk_total_critical': uk_total_critical}
 
     return render(request, "united_kingdom_cases.html", context)
+
+def world_map(request):
+    return render(request, "world.html")
 
 def privacy_policy(request):
     return render(request, "privacy_policy.html")
